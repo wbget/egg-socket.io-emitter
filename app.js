831c34b21cbddb18f058b848d3347fb0fe1b0f56
Webb <xwb1990@gmail.com>
2020-05-05 18:41:11 +0800

    增加ts

--- app.js
+++ app.js
@@ -6,9 +6,7 @@ const io = require('socket.io-emitter');
 module.exports = app => {
   assert(app.redis !== null);
   // 做启动应用前的检查
-  app.didReady(async () => {
-    const emitter = io(app.redis);
-    app.emitter = emitter;
-    app.coreLogger.info('[egg-socket.io-emitter] init success');
-  });
+  const emitter = io(app.redis);
+  app.emitter = emitter;
+  app.coreLogger.info('[egg-socket.io-emitter] init success');
 };
